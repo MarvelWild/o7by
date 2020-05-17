9f1d9a70a9a87c3c322555429a97a689a0ce6516
Marvel Wild <marvel.me@gmail.com>
2020-05-17 08:16:56 +0300

    no message

--- controllers/mag.lua
+++ controllers/mag.lua
@@ -10,15 +10,228 @@ return function(self)
 
 	-- todo: read and store meta
 	local ids={
+		{
+			id='kgN2ekXfuBM',
+			id='Gte3m_-jyCw',
+			id='OD4jKYACQXI',
+			id='SmPToSFKLbk',
+			id='2F6Dhimg4DQ',
+			id='kxJm0iL6M9g',
+			html=[[
+(Verse 1)
+I won't be there, I won't play fair
+In here somewhere, I will find my cause
+I won't be there, no it's not my fair
+I won't  be there, just to lose my cause
+
+Instrumental
+
+(Verse 2)
+I won't be there. (repeat)
+			]]
+		},
 		'-zwVBYpJ3A4',
 		'-9pgIVcB3rk',
 		'KQE29az48gM',
 		'GYecrJwh0QE',
 		'gB_y8LAEVtg',
 		'wpYZsKYUzOI',
-		'fU7hZ3smj0g',
+		{
+			id='fU7hZ3smj0g',
+			html=[[Oh, Lord I'm on a permanent holiday
+I'm going outside to play
+I ain't gonna slave away
+Not for no corporate Babylon
+I'm never gonna be a pawn
+In their manipulation games
+Oh, Lord I'm taking the reins
+I'm breaking the chains
+I'm never gonna kneel, no way
+My prophet is Heaven sent
+No preacher or president can lead I astray
+They'll never know
+I'm taking Jah highway home
+Oh, I've got my own path to follow
+Don't know if you'll overstand
+I've got my, my... my own truth to swallow
+And you know that if I could
+You know I would throw my guitar on my back
+Pick up the slack, and leave here tomorrow
+But I know that I am a pawn of Babylon
+I got to face the facts, embrace the ax
+And cut these chains off my sorrow
+So I'm on a permanent holiday
+I'm going outside to play
+I ain't gonna slave away, no
+Not for no corporate babylon
+I'm never gonna be a pawn
+In their manipulation games
+Ten thousand years of captivity
+We must eventually open up our eyes and see
+They're manipulating we with so much uncertainty
+And so many mysteries
+Why are so few questioning the unnatural state of things?
+It's a nightmare
+We're living in a nightmar
+Everyone's living so scared
+They're virtually unaware
+Of this fear that rules their lives
+Occupies, consumes their minds
+This fear of bankruptcy
+Financial impotancy
+It's funny money, money, money
+It's all this digital currency, woah, yeah
+It's all this monopoly money
+That keeps us from ever being free, ooh-oh-yaeh
+And so it seems we'll be in this prison for life
+'Cause if we keep buying then they'll keep selling the lie
+So it's up to I and I
+I... nip..., mind... and...
+I... Rev..., make... cel...
+I... change... man... words...
+I... be...
+I... own...
+I... (be) nip...(late), mind... and...
+I... (seek) Rev... (lation), make... (life) cel... (bration)
+I... (be) change... (seeking) man... (fast) words... (speak)
+I... (fuse) be... (prisioned)
+I... (make) own... (cisions)
+I (won't be) manip... (ulated), mind (controlled) and in... (undated)
+I (will seek) the reve... (lation), make (my life) a cele... (bration)
+I (will be) the change (I'm seeking), mani... (fest) the words (I'm speaking)
+I (refuse) to be (imprisoned) I (will make) my own (decisions)
+Oh, Lord
+I won't be manipulated
+Mind-controlled and inundated
+I will seek the revelation
+Make my life a celebration
+I'm gonna be the change I'm seeking
+Manifest the words I'm speaking
+I refuse to be imprisoned
+I will make my own decisions
+I'm on a permanent holiday
+I'm going outside to play
+I ain't gonna slave away, no
+Not for no corporate babylon
+I'm never gonna be a pawn
+In their manipulation games
+Oh Lord, I'm taking the reins
+I'm breaking the chains
+I'm never gonna kneel, no way
+My prophet is Heaven sent
+No preacher or president can lead I astray
+We'll never know
+I'm taking Jah highway home
+Ooh, I'll never go astray with na-na-na...
+I'm leaving the past
+And forwarding fast
+'Cause freedom is here to stay
+Oh, Lord we got to take back the knowledge
+We got to take back the power
+We got to take back the knowledge
+We got to take back the power, oh-yeah
+We got to take back the knowledge
+To take back the power
+Take back what they have stolen from our hearts
+And we got to take back the esoteric knowledge
+Oh, Lord for too long they've been keeping us apart
+Oh yes, we got to take back the knowledge
+Take back the power
+Humanity don't let this be our final hour
+Take back the knowledge
+Take back the power
+Humanity don't let this be our final hour
+We got to take back the knowledge
+Take back the power
+Humanity don't let this be our final hour
+Take back the knowledge
+Take back the power
+Humanity don't let this be our final hour
+Take back the knowledge
+Take back the power
+Humanity don't let this be our final hour
+Take back the knowledge
+Take back the power
+Humanity don't let this be our final hour
+Take back the knowledge
+Take back the power
+Humanity don't let this be our final hour
+Take back the knowledge
+Take back the power
+Humanity don't let this be our final hour
+We got take back the knowledge
+Take back the power]]
+		},
 		'icrE3FSxkb4',
-		'BlwrRdiwR74',
+		{
+			id='BlwrRdiwR74',
+			html=[[[Куплет 1]
+Топити за культуру стало модно так часом
+І винити кожного у своїх же невдачах
+Я, як глядач, дивився, аж допоки не настав час
+Коли ви вкотре винуваті, не пробачу знову вас
+І слухай
+
+[Куплет 2]
+Ти крутиш спіннер, я кручу йо-йо
+Я кручу світ цей, ти кажеш йоу-йоу
+Я Прометей - я дарую вогонь
+Ти "Прометей" - лайнове кіно
+І я не про людей, я про ваші потуги
+Ви можливо хороші, та ліхтарики стухли
+Ваше "мистецтво" для поп-сцени і лайножерів
+Ті хто роблять справжню музику ніколи не збрешуть
+Ніби Гей, все для людей (воу)
+Я всім вам розкажу (ага)
+Почекайте будь-ласка, я ці гроші всі зараз у стіл приберу (дідько)
+Стривай-но, постійте, адже це справді кошерно
+Світити мордою в екрані й будувати маєтки
+І ось це ваші думки, рани, шрами душевні
+Чи ви дійсно дурні, ви вважаєте ця гниль і є мистецтво?
+
+[Приспів]
+Я до вас не опущусь, (ні)
+Поки йокає сердце (ей)
+Я до вас не опущусь (ні)
+Поки йокає сердце
+І це мистецтво?
+Я до вас не опущусь, (ні)
+Поки йокає сердце (ага)
+Я до вас не опущусь (ні)
+Поки йокає сердце (дідько)
+І це мистецтво?
+[Куплет 3]
+І, знаєш, знаєш я всім йду наперекір
+Ты слова ковтаєш, а я мчу немов би звір
+Рахуєш гроші, дідько, та який з тебе банкір
+Ти руки ховаєш, ще кишені перевір
+Коли мене помічаєш, то на декілька миль
+Від мене ти тікаєш летять з тебе копійки
+Щось небачу я тих клятих доларів
+Про які ти говорив, ти брехло як і вони
+Немає гідних артистів
+Не захиститися вам, затям
+Ви жалюгідні безкістні
+Мій хист, бійся, він буде завжди йти по головам (дідько)
+І ви риєте, риєте яму молодим
+І ви риєте, риєте, риєте
+І ви риєте, риєте яму мені
+Але самі у ній і згинете
+
+[Пред-Приспів)
+І це мистецтво?
+
+[Приспів]
+Я до вас не опущусь, (ні)
+Поки йокає сердце (ей)
+Я до вас не опущусь (ні)
+Поки йокає сердце
+І це мистецтво?
+Я до вас не опущусь (ні)
+Поки йокає сердце (ага)
+Я до вас не опущусь (ні)
+Поки йокає сердце (дідько)]]
+		},
 		'9fvUHQToXuY',
 		'Zrock_tnsSQ',
 		{
@@ -51,22 +264,320 @@ I don't want to be here alone
 			]]
 		} ,
 		'Eg42h8fgk4k',
-		'bbHZUS5zLhE',
+		{
+			id='bbHZUS5zLhE',
+			html=[[
+Sometimes a man [x4]
+
+You would’ve known I do in the rapture
+There was no way to move any faster
+Because we stay stuck in one place with nothing to do
+And sometimes the sediment is true
+
+Sometimes a man ain’t what he says he is
+Sometimes a man is just a man
+Is just a man, ain’t what he says he is
+
+We celebrate the birth on winter’s pattern
+While really rising Earth to Saturn's power
+And we are slaves trapped inside our own fears
+So misbehave and suffer when we’re dead
+We’re just infected without a cure
+
+Sometimes a man ain’t what he says he is
+Sometimes a man is just a man
+Is just a man, ain’t what he says he is
+
+Sometimes a man is just a man
+Sometimes a man ain’t what he says he is [x2]
+
+Don’t leave me hanging on [x3]
+Don’t leave me hanging today
+
+I don’t want to be left behind
+Everyone’s the same
+God don’t play favorites, right?
+
+Who knows what to believe in anymore? [x2]
+
+Sometimes a man ain’t what he says he is
+Sometimes a man is just a man
+Is just a man, ain’t what he says he is
+
+Don’t leave me hanging on [x7]
+]]
+		},
 		's3YAE_uK8AM',
 		'm53dGt3h9VA',
-		'Lp9GgdCgMXk',
+		{
+			id='Lp9GgdCgMXk',
+			html=[[Hi, hi, howdy, howdy, hi, hi!
+While everyone is minus, you could call me multiply
+Just so you know, yes, yes, I'm that guy
+You could get five fingers and I'm not waving "hi"
+Guess I'm never-ending, you could call me pi
+But really, how long till the world realize
+Yes, yes I'm the best, fuck what you heard
+Anything less is obviously absurd
+Haters get the bird, more like an eagle
+This is my movie, stay tuned for the sequel
+Seems so wrong, seems so illegal
+Fellas in the back like a foul-ball free-throw
+Yep, yep, you know that I go
+This is me on the regular, so you know
+This is me on the regular, so you know
+Yep, yep, you know that I go
+This is me on the regular, so you know
+This is me on the regular, so you know
+I come with the tip, with the blow, with the boom
+And if you're in my way, there's nothing but doom
+Ain't got no time for you ratchet-ass goons
+And just settle down, listen to my tunes
+Ever since I was eight I was attached to the mic
+Wanted a guitar before I wanted a bike
+Had an Apple phone, fuck a Fisher-Price
+Never see the sun 'cause I'm up all night (What)
+Really, really? Really, really?
+You want to talk shit but you know that I am illy?
+Illy to the fullest, you can call me cancer
+No multiple choice, 'cause I'm the only answer
+Ain't got no wallet, only use rubber bands
+You know my chick is ratchet 'cause that's what make a man
+You wanna get at me, but you don't stand a chance
+And if you wanna fuck, yes you can take your chance
+Haters get the bird, more like an eagle
+This is my movie, stay tuned for the sequel
+Seems so wrong, seems so illegal
+Fellas in the back like a foul-ball free-throw
+Yep, yep, you know that I go
+This is me on the regular, so you know
+This is me on the regular, so you know
+Yep, yep, you know that I go
+This is me on the regular, so you know
+This is me on the regular, so you know
+Five-foot-ten, about a buck twenty
+Skinny as a rod but still wanna fuck with me
+Wise ass nigga, you'd think I was fifty
+But I take your fifty after you take this love see
+Yep, yep, you know that I go
+This is me on the regular, so you know
+This is me on the regular, so you know
+Don't try me, I'm not a free sample
+Step to me and you will be handled
+See, that's my crown on the mantle
+And if you try to touch it, yes, there will be a scandal
+Don't try me, I'm not a free sample
+Step to me and you will be handled
+See, that's my crown on the mantle
+And if you try to touch it, yes, there will be a scandal
+This is the end, but you really feeling it
+Here's one more verse, 'cause I ain't no sleepy bitch
+Ain't got much to say 'cause it's in my piece
+Guess more hit the blunt and I ain't talking weed
+Yep, yep, you know that I go
+This is me on the regular, so you know
+This is me on the regular, so you know
+Yep, yep, you know that I go
+This is me on the regular, so you know
+This is me on the regular, so you know]]
+		},
 		'42JxVqZ9QFo',
-		'EBx_FuZC9Q0',
+		{
+			id='EBx_FuZC9Q0',
+			html=[[I want a brand new house
+Something I cannot buy, something I can't afford
+I want a brand new house
+Something I cannot buy, something I can't afford
+I want a brand new house
+Something I cannot buy, something I can't afford
+I want a brand new house
+Something I cannot buy, something I can't afford
+
+I ain't even make it off the jetway (damn)
+Phones been on blast like all day (ring)
+Why you gotta do this, try to test me (now)
+Right when I touchdown got anxiety (fuck)
+Follow signs out of the terminal (now)
+JFK is a different animal now
+Damn, baggage claims like a warzone (now)
+Glad I packed light 'cause I'm on my own
+
+I want a brand new house
+Something I cannot buy, something I can't afford
+I want a brand new house
+Something I cannot buy, something I can't afford
+I want a brand new house
+Something I cannot buy, something I can't afford
+I want a brand new house
+Something I cannot buy, something I can't afford
+
+I just want a long shower
+I been feeling so crowded
+My bad I think I think I saw your text
+My bad here's my new address
+Stay for me, stay for me
+Is there a reasoning?]]
+		},
 		'bqdbYEaph9Q',
 		'25MziL_5BJA',
 		'wR5zlx56VkA',
 		'k_oTQd93eRI',
 		'JZ6ZzJeWgpY',
+		{
+			id='XiSLXxSNKkI',
+			html=[[Why don't we...Why don't we
+Why don't we...Why don't we
+Take me away
+Release me from the cage
+Free me from the pain
+And let me feel no shame
+The stars spin circles around the bad times
+You splashed your water on my fire sign
+The fortune teller told us to unwind
+Come on run away
+Runaway...Runaway...Runaway
+Why don't we
+Why don't we
+I've felt invisible for a long time
+This town will smother what's left of mine
+Untaken chances come back to life
+Come on run away
+Runaway...Runaway...Runaway
+I've been I've been all
+I've been I've been all
+I've been I've been all around the world dream
+Let's go free...Let's go free
+Why don't we...Why don't we
+You've been saying that you want to go
+I'm finally ready to let go
+Let's go...Let's go...Let's go
+I am finally ready
+Take me away
+Release me from the cage
+Free me from the pain
+And let me feel no shame
+Let's go free...Let's go free
+Runaway]]
+		},
 		'ftqkP3n2qLU',
-		'QyZeJr5ppm8',
+		{
+			id='QyZeJr5ppm8',
+			html=[[I feel your love
+I feel time is up
+When I was a child, I never knew enough
+What that do to me?
+The wave of love is a transient hunt
+Water's the shell and we are the nut
+But I saw a hand arch out of the barrel
+Look at all the peaches
+How do you celebrate?
+Can't appear inside of nowhere
+It's already dead
+I know you have the dove
+I'm not getting wet
+Looks like a date is set
+Show the ferret to the egg
+I'm not gettin' led along
+I rushed in to hold down your page
+And now I sleep 'side words you do not read with me
+I hear a song from inside the maze, the very one you made
+You shook at the ivory mantle
+As a poet, I knew to be gentle
+When you have a child, so begins the braiding
+And in that braid you stay
+Look at all the peaches
+I want to celebrate
+I can appear inside of nowhere
+Look at all the peaches
+It's already dead
+I know you have the dove
+I'm not getting wet
+Looks like a date is set
+Show the ferret to the egg
+I'm not getting led along
+The wave of love is a transient hunt
+Water's the shell and we are the nut
+But I saw a match struck outside the barrel
+It's already dead
+I know you have the dove
+I'm not getting wet
+(The barrel)
+(The barrel)
+Looks like a date is set
+Show the ferret to the egg
+I'm not getting led along
+(The barrel)
+(The barrel)
+It's already dead
+I know you have the dove
+I'm not getting wet
+(The barrel)
+(The barrel)
+Looks like a date is set
+Show the ferret to the egg
+I'm not getting led along
+(The barrel)
+(The barrel)
+(The barrel)
+(The barrel)
+(The barrel)
+(The barrel)]]
+		},
 		'ZslD5_x0UTU',
 		'_k1y8pymrF4',
 		--		'Au46lJu8icA',
+		{
+			id='etviGf1uWlg',
+			html=[[Dig
+The chills that you spill up my back
+Keep me filled with satisfaction when we're done
+Satisfaction of what's to come
+I couldn't ask for another
+No, I couldn't ask for another
+Your groove, I do deeply dig
+No walls, only the bridge, my supper dish
+My succotash wish
+I couldn't ask for another
+No, I couldn't ask for another
+Groove is in the heart, Groove is in the heart
+Groove is in the heart, Groove is in the heart
+Watch out!
+The depth of hula groove
+Move us to the nth hoop
+We're going through to, Horten hears a who
+I couldn't ask for another
+No, I couldn't ask for another
+DJ Soul was on a roll
+I been told he can't be sold
+Not vicious or malicious
+Just de-lovely and delicious
+I couldn't ask for another
+Someone's in this torso (yeah)
+Hot! Gotta deal you wanna know (wanna know)
+Dee-Liteful truly Dee-Liteful (liteful)
+Makin' it doin' it 'specially at show (show)
+Feeling kinda high like a Hendrix haze (haze)
+Music makes motion moves like a maze (maze)
+All inside of me (side)
+Heart especially (yeah)
+Help of the rhythm where I wanna be (c'mon)
+Flowin' glowin' with electric eyes (ha)
+You dip to the dive baby you'll realize (yeah)
+Baby you'll see the funky side of me
+Baby you'll see that rhythm is the key (hmm!)
+Get get ready with it (with it)
+Can't can't quit it (quit it)
+Stomp on the street when I hear a funk beat (beat)
+Playing Pied Piper
+Follow what's true
+Baby just sing about the groove (sing it)
+Groove is in the heart
+Groove is in the heart
+Groove is in the heart
+Groove is in the heart
+Groove is in the heart
+			]]
+		},
 	}
 	--self.ids=ids
 
